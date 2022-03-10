fe6dcae7ab851ae13111a9353d50e41bfebf0efb
ZexUlt <danil.chernozubov66@gmail.com>
2022-03-11 00:51:51 +0300

    Added sigmoidal activation function

---
+++ SigmoidActivation.cpp
@@ -0,0 +1,16 @@
+#include "SigmoidActivation.hpp"
+
+using CxxSDNN::SigmoidActivation;
+
+SigmoidActivation::SigmoidActivation(double a, double b, double c, double d) :
+    param_a{a}, param_b{b}, param_c{c}, param_d{d}
+{
+
+}
+
+nc::NdArray<double> SigmoidActivation::map(nc::NdArray<double> input, double step)
+{
+    return this->param_a / (
+        this->param_b + this->param_c * nc::exp(this->param_d * input)
+        );
+}
