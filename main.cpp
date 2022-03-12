c7184a3e99f7854ecff9315aa60e6e8a6f8daab2
ZexUlt <danil.chernozubov66@gmail.com>
2022-03-12 22:47:00 +0300

    DNN testing preparations

--- main.cpp
+++ main.cpp
@@ -1,47 +1,22 @@
 #include "NumCpp.hpp"
-#include "Utility.hpp"
+#include "SpikeDNNet.hpp"
+#include "IzhikevichActivation.hpp"
 #include <iostream>
 
 int main(int argc, char** argv)
 {
-    nc::DataCube<double> dc(3305);
+    const nc::uint32 dim = 2u;
 
-    auto a = UtilityFunctionLibrary::construct_fill_DC(nc::ones<int>(2), 3);
-    std::cout << a.sizeZ() << '\n';
+    CxxSDNN::IzhikevichActivation izh_act_1(dim);
+    CxxSDNN::IzhikevichActivation izh_act_2(dim);
 
-    for(auto i = 0; i < 3; ++i){
-        std::cout << a[i] << " ";
-    }
+    CxxSDNN::SpikeDNNet dnn(
+        izh_act_1, izh_act_2, // Activation functions
+        20. * nc::ones<double>(2), 20. * nc::ones<double>(2), // W_1, W_2
+        dim, 20. * nc::diag<double>({-1., -2.}), // dim, mat_A
+        1575.9 * nc::diag<double>({60., 40,}), .15 * nc::diag<double>({10., 1.}), // mat_P, K_1
+        .15 * nc::diag<double>({1., 1.}) // K_2
+    );
     
-    // a.push_back(nc::NdArray<int>{{5,6}, {7,8}});
-    // a.push_back(nc::NdArray<int>{{9,10}, {11,12}});
-
-    // for(int i = 0; i < 3; ++i){
-    //     std::cout << a[i] << '\n';
-    // }
-
-    // std::cout << a.sliceZAll(0, 0) << '\n' << a.sliceZAll(0,1) <<
-    // '\n' << a.sliceZAll(1,0) << '\n' << a.sliceZAll(1,1) << '\n';
-    // std::cout << UtilityFunctionLibrary::convolveValid(nc::NdArray<double>{3,3,3,4}, nc::NdArray<double>{5,3,1,1}) << '\n';
-    
-    // for(int i = 0; i < 3304; ++i){
-    //     dc.push_back(nc::ones<double>(2));
-    // }
-
-    // std::cout << dc.sizeZ() << '\n';
-    // std::cout << dc[0] << '\n';
-    
-    // nc::DataCube<double> new_x(3304);
-
-    // for(int i = 0; i < 3304; ++i){
-    //     new_x.push_back(nc::ones<double>(2));
-    // }
-
-    // for(int i = 0; i < 2; ++i){
-    //     for(int j = 0; j < 2; ++j){
-    //         new_x.s
-    //     }
-    // }
-
     return 0;
 }
