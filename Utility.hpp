e288e406f0dff5fce8051bb5d730b3a42324f77c
ZexUlt <danil.chernozubov66@gmail.com>
2022-04-05 00:20:11 +0300

    Removed dependency on the matplot++ lib & removed plot_article method

--- Utility.hpp
+++ Utility.hpp
@@ -2,13 +2,11 @@
 
 #include "NumCpp.hpp"
 #include "SpikeDNNet.hpp"
-#include "matplot/matplot.h"
 
 #include <map>
 #include <string>
 #include <iosfwd>
 
-namespace plt = matplot;
 
 class UtilityFunctionLibrary
 {
@@ -21,6 +19,10 @@ public:
         std::pair<double, double> mse_res;
         std::pair<double, double> mae_res;
         std::pair<double, double> smae_res;
+        std::vector<nc::NdArray<double>> tr_est;
+        std::vector<nc::NdArray<double>> vl_res;
+        nc::DataCube<double> W_1;
+        nc::DataCube<double> W_2;
 
         friend std::ostream& operator<<(std::ostream& out, UtilityFunctionLibrary::ValidationResults res)
         {
@@ -78,6 +80,8 @@ public:
 
         auto target_est = dnn.fit(tr_target, tr_control);
 
+        std::cout << target_est;
+
         auto vl_pred = dnn.predict(nc::NdArray<double>({target_est(target_est.shape().rows - 1, 0)}), vl_control);
 
         results.mse_res = std::make_pair(
@@ -94,6 +98,11 @@ public:
             results.mae_res.first / nc::mean(tr_target(tr_target.rSlice(), 0))[0],
             results.mae_res.second / nc::mean(vl_target(vl_target.rSlice(), 0))[0]
         );
+
+        results.tr_est.emplace_back(target_est);
+        results.vl_res.emplace_back(vl_pred);
+        results.W_1 = dnn.get_weights(0);
+        results.W_2 = dnn.get_weights(1);
 
         return results;
     }
@@ -113,54 +122,4 @@ public:
 
         return nc::average(output_errors)[0];
     }
-
-    static void plot_article(
-        nc::int32 idx, nc::NdArray<double> time, nc::int32 split,
-        nc::int32 width, nc::NdArray<double> tr_target, nc::NdArray<double> tr_control,
-        nc::NdArray<double> vl_target, nc::NdArray<double> vl_control, 
-        nc::NdArray<double> tr_est, nc::NdArray<double> error,
-        std::vector<nc::DataCube<double>> weights_dyn)
-    {
-        std::vector<plt::axes_handle> ax;
-        for (size_t i = 0; i < 4; ++i) {
-            ax.emplace_back(plt::subplot(2, 2, i));
-        }
-
-        plt::subplot(ax[0]);
-        plt::plot(
-            (time({0, split}, time.cSlice()) / 110.).toStlVector(), 
-            nc::degrees(tr_control).toStlVector()
-        )->line_width(2.).color({0., 0., 255.});
-
-        plt::subplot(ax[1]);
-        plt::scatter(
-            (time({0, split}, time.cSlice()) / 110.).toStlVector(),
-            (nc::degrees(tr_target(tr_target.rSlice(), 0)) + 2.).toStlVector()
-        )->line_style("solid").color({165./255, 172./255, 175./255});
-
-        plt::subplot(ax[1]);
-        plt::plot(
-            (time({0, split}, time.cSlice()) / 110.).toStlVector(),
-            (nc::degrees(tr_est(tr_est.rSlice(), 0)) + 2.).toStlVector()
-        )->line_width(2.).color({0., 0., 255.});
-
-        plt::subplot(ax[2]);
-        plt::plot(
-            (time({0, split}, time.cSlice()) / 110.).toStlVector(),
-            nc::degrees(error).toStlVector()
-        )->line_width(2.).color({0., 0., 255.});
-
-        plt::subplot(ax[3]);
-        plt::plot(
-            (time({0, split - 2}, time.cSlice()) / 110.).toStlVector(),
-            nc::abs(weights_dyn[0].sliceZAll(1, {0, split-2})).toStlVector()
-        )->line_width(2.).color({165./255, 172./255, 175./255});
-        
-        plt::subplot(ax[3]);
-        plt::plot(
-            (time({0, split - 2}, time.cSlice()) / 110.).toStlVector(),
-            nc::abs(weights_dyn[1].sliceZAll(1, {0, split-2})).toStlVector()
-        )->line_width(2.).color({0., 0., 255.});
-    }
-
 };
