34be227556349f9a75d8dc47538da0fd63c732d5
ZexUlt <danil.chernozubov66@gmail.com>
2022-04-13 18:49:56 +0300

    Added ability of addition of CMake options

--- configure.sh
+++ configure.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 RECONFIG=0
+CMAKE_PARAMETERS=()
 
-if [ $# -gt 0 ]; then
+while [[ $# -gt 0 ]]; do
     case $1 in
         -r|--reconfigure)
             RECONFIG=1
+            shift # past option
             ;;
         -h|--help)
             echo "Configuration script."
@@ -14,17 +16,22 @@ if [ $# -gt 0 ]; then
             echo "       -h, --help        = Show this help and exit."
             exit 0
             ;;
+        -D*)
+            CMAKE_PARAMETERS+=( $1 )
+            shift
+            ;; 
         -*|--*)
             echo "Unknown option $1"
             exit 1
             ;;
     esac
-fi
+done
 
-if [ ${RECONFIG} -eq 1 ]; then
+if [[ ${RECONFIG} -eq 1 ]]; then
     echo "Cleaning previous configuration data..."
     rm -r ./build
 fi
 
+echo "CMAKE_PARAMETERS = [$CMAKE_PARAMETERS]"
 echo "Building..."
-cmake -S . -B ./build
+cmake $CMAKE_PARAMETERS -S . -B ./build 
