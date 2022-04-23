6481862f1623350a6917de7484a6b30ee369b34e
ZexUlt <danil.chernozubov66@gmail.com>
2022-04-23 23:59:18 +0300

    Added debug macros

--- include/debug_header.hpp
+++ include/debug_header.hpp
@@ -1,2 +1,6 @@
 #include <iostream>
 #include <string>
+
+#define DEBUG_SHAPE(x) std::cout << #x << " " << x.shape()[0] << " " << x.shape()[1] << "\n" << x.dimension() << "\n"
+
+#define DEBUG_XARRAY(x) std::cout << #x << " " << x << '\n'
