1369a088521040ae66c7b5d1408ffc539c627719
ZexUlt <danil.chernozubov66@gmail.com>
2022-05-01 18:59:26 +0300

    [New] Renamed DEBUG_XARRAY -> DEBUG_PRINT [New] Provided 3 shapes in DEBUG_SHAPE

--- include/debug_header.hpp
+++ include/debug_header.hpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
 
-#define DEBUG_SHAPE(x) std::cout << #x << " " << x.shape()[0] << " " << x.shape()[1] << "\n" << x.dimension() << "\n"
+#define DEBUG_SHAPE(x) std::cout << #x << " " << x.shape()[0] << " " << x.shape()[1] << " " << x.shape()[2] << "\n" << x.dimension() << "\n"
 
-#define DEBUG_XARRAY(x) std::cout << #x << " " << x << '\n'
+#define DEBUG_PRINT(x) std::cout << #x << " " << x << '\n'
