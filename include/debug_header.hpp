5b98d95335f052577c311cefa455f4c088c4fd9c
ZexUlt <danil.chernozubov66@gmail.com>
2022-05-09 00:41:57 +0300

    Rewrited DEBUG_WHERE macro

--- include/debug_header.hpp
+++ include/debug_header.hpp
@@ -4,3 +4,5 @@
 #define DEBUG_SHAPE(x) std::cout << #x << " " << x.shape()[0] << " " << x.shape()[1] << " " << x.shape()[2] << "\n" << x.dimension() << "\n"
 
 #define DEBUG_PRINT(x) std::cout << #x << " " << x << '\n'
+
+#define DEBUG_WHERE std::cout << __FILE__ << ": " << __LINE__ << '\n'
