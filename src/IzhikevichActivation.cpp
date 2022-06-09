189186358fe9a6356200640034e96e4865c022a2
ZexUlt <danil.chernozubov66@gmail.com>
2022-06-09 23:03:47 +0300

    Testing pre-commit hook

--- src/IzhikevichActivation.cpp
+++ src/IzhikevichActivation.cpp
@@ -114,7 +114,6 @@ void IzhikevichActivation::set_type(NeuronType newType)
 const std::string IzhikevichActivation::whoami() const
 {
   std::string out;
-  
 
   out += "\tIzhikevich neuron pack\n\tShape: {";
   for(auto x : this->shape) {
