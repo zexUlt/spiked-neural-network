cb07ec93bb9e926e69652b6f7243da36022770e5
ZexUlt <danil.chernozubov66@gmail.com>
2022-06-11 23:48:07 +0300

    Updated command-line parameters to clang-tidy

--- cmake/clang-cxx-dev-tools.cmake
+++ cmake/clang-cxx-dev-tools.cmake
@@ -11,18 +11,14 @@ if(CLANG_TIDY)
     if(APPLY_FIX)
         add_custom_target(clang-tidy
             COMMAND /usr/bin/clang-tidy
-            -config=''
-            -header-filter=.*
             --fix
             -p=${PROJECT_SOURCE_DIR}/build
             --extra-arg=-std=c++17
-            ${ALL_CXX_SOURCE_FILES}
+            ${ALL_CXX_SOURCE_FILES} ${PROJECT_SOURCE_DIR}/main.cpp
         )
     else()
         add_custom_target(clang-tidy
             COMMAND /usr/bin/clang-tidy
-            -config=''
-            -header-filter=.*
             -p=${PROJECT_SOURCE_DIR}/build
             --extra-arg=-std=c++17
             ${ALL_CXX_SOURCE_FILES} ${PROJECT_SOURCE_DIR}/main.cpp
