da125fa19139e45eb94374356bda47b3a9946dcd
ZexUlt <danil.chernozubov66@gmail.com>
2022-06-26 01:43:37 +0300

    Created base PCH file

---
+++ include/precompiled.hpp
@@ -0,0 +1,17 @@
+#pragma once 
+
+#include <vector>
+#include <xtensor/xarray.hpp>
+
+#include "xtensor-blas/xlinalg.hpp"
+#include "xtensor/xnpy.hpp"
+#include "xtensor/xview.hpp"
+
+#include <chrono>
+#include <map>
+#include <string>
+#include <fstream>
+#include <istream>
+#include <unordered_map>
+#include <memory>
+#include <xtensor/xio.hpp>
