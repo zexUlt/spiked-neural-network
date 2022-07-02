2b37b920c9bfdca4e4f043a416bcf2ca83f64108
ZexUlt <danil.chernozubov66@gmail.com>
2022-07-02 22:11:48 +0300

    Collection of functions for Python bindings

---
+++ include/C_API/c_api.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+extern "C"
+{
+    double min_functional(double a, double p, double k1, double k2, double alpha, const char* neuronType);
+}
---
+++ src/c_api.cpp
@@ -0,0 +1,11 @@
+#include "c_api.hpp"
+#include "Optimization.hpp"
+#include "SpikeDNNet.hpp"
+
+extern "C"
+{
+    double min_functional(double a, double p, double k1, double k2, double alpha, const char* neuronType)
+    {
+        return cxx_sdnn::optimization::run_minimize(a, p, k1, k2, alpha, neuronType);
+    }
+}
