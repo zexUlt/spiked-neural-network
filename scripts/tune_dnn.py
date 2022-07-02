49de1e6beaae237f947c7a8ef965215d5071e96b
ZexUlt <danil.chernozubov66@gmail.com>
2022-07-02 23:33:31 +0300

    [WIP] Python bindings for C++ code with cppyy

--- scripts/tune_dnn.py
+++ scripts/tune_dnn.py
@@ -9,20 +9,30 @@ LIB_NAME = 'libspiked-neural-network.so'
 PROJECT_ROOT_DIR = pathlib.Path().absolute()
 PROJECT_INCLUDE_DIR = PROJECT_ROOT_DIR / "include"
 
-def run_dnn(func: Callable, x0: List[float], act_type: str) -> float:
-    return func(*x0, act_type)
 
 if __name__ == '__main__':
     lib_path = PROJECT_ROOT_DIR / PATH_TO_DLL
-    
-    # cppyy.include(PROJECT_INCLUDE_DIR / "Optimization/Optimization.hpp")
     cppyy.add_library_path(str(PATH_TO_DLL))
     cppyy.add_include_path(str(PROJECT_INCLUDE_DIR))
     cppyy.load_library(LIB_NAME)
+
+
     cppyy.include("Optimization/Optimization.hpp")
-    # act_type = "sigmoid".encode("utf-8")
-    # x0 = [1000, 500, 1, 0.9]
+    cxx_opt = cppyy.gbl.cxx_sdnn.optimization
+    cppyy.cppexec("std::string neuronType;")
+    cppyy.gbl.neuronType = "sigmoid"
+    # param_pack = [1000., 500., 1., 0.9, 1.]
+    
+    cppyy.cppexec("cxx_sdnn::optimization::TrainedParams params{1000., 500., 1., 0.9, 1.};")
+    # trainedParams = cxx_opt.TrainedParams
+    # trainedParams.a = 1000.
+    # trainedParams.p = 500.
+    # trainedParams.k1 = 1.
+    # trainedParams.k2 = 0.9
+    # trainedParams.alpha = 1.
 
-    # ret = run_dnn(functional, x0, act_type)
+    cppyy.cppexec("std::string trainRoot;")
+    cppyy.gbl.trainRoot = "./train_data/"
+    print(cxx_opt.estimate_loss(cppyy.gbl.trainRoot, cppyy.gbl.params, cppyy.gbl.neuronType))
+    # print(cxx_opt.run_minimize(1000., 500., 1., 0.9, 1.,cppyy.gbl.neuronType))
 
-    # print(ret)
