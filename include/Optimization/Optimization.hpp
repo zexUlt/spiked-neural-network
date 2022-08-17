59165d4508d4b07bee53739b8734917252be81d3
ZexUlt <danil.chernozubov66@gmail.com>
2022-08-17 22:52:10 +0300

    Finilazed optimization module

--- include/Optimization/Optimization.hpp
+++ include/Optimization/Optimization.hpp
@@ -13,12 +13,17 @@ namespace cxx_sdnn
       double k1;
       double k2;
       double alpha;
+      double sigm_a;
+      double sigm_b;
+      double sigm_c;
+      double sigm_d;
+      double sigm_e;
     };
 
     auto
     setup_dnn(std::uint32_t targetDim, std::uint32_t controlDim, const TrainedParams& params, std::string neuronType);
     double estimate_loss(std::string trainingDataRoot, TrainedParams params, std::string neuronType);
-    double run_minimize(double a, double p, double k1, double k2, double alpha, std::string neuronType);
+    double run_minimize(std::vector<double> intialParams, std::string neuronType, std::string trainingDataRoot);
     void dummy();
   }; // namespace optimization
 };   // namespace cxx_sdnn
--- src/Optimization.cpp
+++ src/Optimization.cpp
@@ -3,81 +3,94 @@
 #include "IzhikevichActivation.hpp"
 #include "SigmoidActivation.hpp"
 #include "SpikeDNNet.hpp"
-#include "Utility.hpp"
-#include "vector"
+#include "Utility/Utility.hpp"
+#include "debug_header.hpp"
 
 #include "xtensor/xarray.hpp"
 
-namespace cxx_sdnn::optimization
+#include <memory>
+#include <vector>
+
+namespace cxx_sdnn
 {
-  auto setup_dnn(std::uint32_t targetDim, std::uint32_t controlDim, const TrainedParams& params, std::string neuronType)
+  namespace optimization
   {
-    using Izhi = IzhikevichActivation;
+    auto
+    setup_dnn(std::uint32_t targetDim, std::uint32_t controlDim, const TrainedParams& params, std::string neuronType)
+    {
+      using Izhi = IzhikevichActivation;
 
-    std::unordered_map<std::string, xt::xarray<double>> modelParams{
-      {"W_1", 1000. * xt::ones<double>({targetDim, targetDim})},
-      {"W_2", 1000. * xt::ones<double>({targetDim, targetDim})},
-      {"A", params.a * xt::diag(xt::xarray<double>{-1., -1., -1., -1.})},
-      {"P", params.p * xt::diag(xt::xarray<double>{1., 1., 1., 1.})},
-      {"K_1", params.k1 * xt::diag(xt::xarray<double>{1., 1., 1., 1.})},
-      {"K_2", params.k2 * xt::diag(xt::xarray<double>{1., 1., 1., 1.})}};
+      std::unordered_map<std::string, xt::xarray<double>> modelParams{
+        {"W_1", 1000. * xt::ones<double>({targetDim, targetDim})},
+        {"W_2", 1000. * xt::ones<double>({targetDim, targetDim})},
+        {"A", params.a * xt::diag(xt::xarray<double>{-1., -1., -1., -1.})},
+        {"P", params.p * xt::diag(xt::xarray<double>{1., 1., 1., 1.})},
+        {"K_1", params.k1 * xt::diag(xt::xarray<double>{1., 1., 1., 1.})},
+        {"K_2", params.k2 * xt::diag(xt::xarray<double>{1., 1., 1., 1.})}};
 
-    if(neuronType == "izhikevich") {
-      auto izhAct1 = make_izhikevich(50, 1 / 40., {2 * targetDim, 1}, Izhi::NeuronType::RESONATOR);
-      auto izhAct2 = make_izhikevich(55, 1 / 40., {2 * targetDim, controlDim}, Izhi::NeuronType::THALAMO_CORTICAL63);
+      if(neuronType == "izhikevich") {
+        auto izhAct1 = cxx_sdnn::make_izhikevich(50, 1 / 40., {2 * targetDim, 1}, Izhi::NeuronType::RESONATOR);
+        auto izhAct2 =
+          cxx_sdnn::make_izhikevich(55, 1 / 40., {2 * targetDim, controlDim}, Izhi::NeuronType::THALAMO_CORTICAL63);
 
-      auto dnn =
-        UtilityFunctionLibrary::make_dnn(params.alpha, targetDim, std::move(izhAct1), std::move(izhAct2), modelParams);
+        izhAct1->set_integration_step(0.0001);
+        izhAct2->set_integration_step(0.0001);
 
-      return dnn;
-    } else {
-      auto sigmAct1 = std::make_unique<cxx_sdnn::SigmoidActivation>(
-        /*shape=*/std::vector<size_t>{targetDim, 1},
-        /*a =*/1.,
-        /*b =*/1.,
-        /*c =*/1,
-        /*d =*/-1,
-        /*e =*/-0.5);
+        auto dnn = cxx_sdnn::UtilityFunctionLibrary::make_dnn(
+          params.alpha, targetDim, std::move(izhAct1), std::move(izhAct2), modelParams);
 
-      auto sigmAct2 = std::make_unique<cxx_sdnn::SigmoidActivation>(
-        /*shape=*/std::vector<size_t>{targetDim, controlDim},
-        /*a =*/1.,
-        /*b =*/1.,
-        /*c =*/1.,
-        /*d =*/-1,
-        /*e =*/-0.5);
+        return dnn;
+      } else {
+        auto sigmAct1 = std::make_unique<cxx_sdnn::SigmoidActivation>(
+          /*shape=*/std::vector<size_t>{targetDim, 1},
+          /*a =*/params.sigm_a,
+          /*b =*/params.sigm_b,
+          /*c =*/params.sigm_c,
+          /*d =*/params.sigm_d,
+          /*e =*/params.sigm_e);
 
-      auto dnn = UtilityFunctionLibrary::make_dnn(
-        params.alpha, targetDim, std::move(sigmAct1), std::move(sigmAct2), modelParams);
+        auto sigmAct2 = std::make_unique<cxx_sdnn::SigmoidActivation>(
+          /*shape=*/std::vector<size_t>{targetDim, controlDim},
+          /*a =*/params.sigm_a,
+          /*b =*/params.sigm_b,
+          /*c =*/params.sigm_c,
+          /*d =*/params.sigm_d,
+          /*e =*/params.sigm_e);
+          
+        auto dnn = cxx_sdnn::UtilityFunctionLibrary::make_dnn(
+          params.alpha, targetDim, std::move(sigmAct1), std::move(sigmAct2), modelParams);
 
-      return dnn;
+        return dnn;
+      }
     }
-  }
 
-  double estimate_loss(std::string trainingDataRoot, TrainedParams params, std::string neuronType)
-  {
-    static auto trainData  = UtilityFunctionLibrary::prepare_dataset(trainingDataRoot)["tr"];
-    static auto targetDim  = trainData.first.shape(1);
-    static auto controlDim = trainData.second.shape(1);
+    double estimate_loss(std::string trainingDataRoot, TrainedParams params, std::string neuronType)
+    {
+      static auto trainData  = cxx_sdnn::UtilityFunctionLibrary::prepare_dataset(trainingDataRoot)["tr"];
+      static auto targetDim  = trainData.first.shape(1);
+      static auto controlDim = trainData.second.shape(1);
 
-    const double STEP            = 0.01;
-    const std::uint32_t EPOCHS   = 1u;
-    const std::uint32_t K_POINTS = 2u;
-    auto dnn                     = setup_dnn(targetDim, controlDim, params, neuronType);
+      const double STEP            = 0.01;
+      const std::uint32_t EPOCHS   = 1u;
+      const std::uint32_t K_POINTS = 2u;
+      auto dnn                     = setup_dnn(targetDim, controlDim, params, neuronType);
 
-    dnn->fit(trainData.first, trainData.second, STEP, EPOCHS, K_POINTS);
+      dnn->fit(trainData.first, trainData.second, STEP, EPOCHS, K_POINTS);
 
-    return dnn->integral_loss();
-  }
+      return dnn->integral_loss();
+    }
 
-  double run_minimize(double a, double p, double k1, double k2, double alpha, std::string neuronType)
-  {
-    TrainedParams params{a, p, k1, k2, alpha};
-    return estimate_loss("", params, neuronType);
-  }
+    double run_minimize(std::vector<double> initialParams, std::string neuronType, std::string trainingDataRoot)
+    {
+      TrainedParams params{
+        initialParams[0], initialParams[1], initialParams[2], initialParams[3], initialParams[4],
+        initialParams[5], initialParams[6], initialParams[7], initialParams[8], initialParams[9]};
+      return estimate_loss(trainingDataRoot, params, neuronType);
+    }
 
-  void dummy()
-  {
-    std::cout << "Hello World!\n";
-  }
-}; // namespace cxx_sdnn::optimization
+    void dummy()
+    {
+      std::cout << "Hello World!\n";
+    }
+  } // namespace optimization
+};  // namespace cxx_sdnn
