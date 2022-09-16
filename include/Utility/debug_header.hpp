f08d16c8a8cbe2e24ba02e4a94aefe78694cbfe8
ZexUlt <danil.chernozubov66@gmail.com>
2022-09-16 23:21:32 +0300

    no-op for header if it's not Debug build

--- include/Utility/debug_header.hpp
+++ include/Utility/debug_header.hpp
@@ -1,9 +1,15 @@
-#include <iostream>
-#include <string>
+#ifndef NDEBUG
+  #include <iostream>
+  #include <string>
 
-#define DEBUG_SHAPE(x) \
-  std::cout << #x << " " << x.shape()[0] << " " << x.shape()[1] << " " << x.shape()[2] << "\n" << x.dimension() << "\n"
+  #define DEBUG_SHAPE(x) \
+    std::cout << #x << " " << x.shape()[0] << " " << x.shape()[1] << " " << x.shape()[2] << "\n" << x.dimension() << "\n"
 
-#define DEBUG_PRINT(x) std::cout << #x << " " << x << '\n'
+  #define DEBUG_PRINT(x) std::cout << #x << " " << x << '\n'
 
-#define DEBUG_WHERE std::cout << __FILE__ << ": " << __LINE__ << '\n'
+  #define DEBUG_WHERE std::cout << __FILE__ << ": " << __LINE__ << '\n'
+#else
+  #define DEBUG_SHAPE(x) ((void)0)
+  #define DEBUG_PRINT(x) ((void)0)
+  #define DEBUG_WHERE ((void)0)
+#endif
